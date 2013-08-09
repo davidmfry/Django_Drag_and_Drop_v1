033953f5edb185550fd492a0f529bb3a171c1900
David Fry <david.fry.tv@gmail.com>
2013-08-09 10:42:50 -0700

    Fresh Install

---
+++ django_1.5/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Volumes/MEDIA_HD/DAVID/Projects/Apps_in_progress/Django_drag_and_drop/django_1.5/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ drag_and_drop_v1/drag_and_drop_v1/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'drag_and_drop_v1.views.home', name='home'),
+    # url(r'^drag_and_drop_v1/', include('drag_and_drop_v1.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
